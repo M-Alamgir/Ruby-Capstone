09786ce81cbb4eb605d23892403ce247523d7913
Alamgir <ourandoc@gmail.com>
2021-01-27 20:00:03 +0500

    Fix trailing whitespace method

--- bin/main.rb
+++ bin/main.rb
@@ -4,4 +4,10 @@ require_relative "../lib/load_file.rb"
 require_relative "../lib/check_linters.rb"
 
 check = Check_linters.new
-puts check.trailing_space_error
+check.trailing_space_error
+
+if check.offenses.empty?
+    puts 'No offenses'.colorize(:green) + ' detected'
+else
+    puts check.offenses
+end
--- lib/check_linters.rb
+++ lib/check_linters.rb
@@ -2,16 +2,17 @@ require 'colorize'
 require_relative "load_file.rb"
 
 class Check_linters < Load_file
+    attr_accessor :no_offense, :offenses
     def initialize
         @load_file = Load_file.new('test/*.rb')
         @read_file = @load_file.read_file
-        @offense = []
+        @offenses = []
     end
 
     def trailing_space_error
         @load_file.file.readlines.each_with_index do | val, index |
             if val[-2] == ' ' && !val.strip.empty?
-                return "At line #{index + 1}:#{val.size - 1} Trailing Whitespace Detected".colorize(:red)
+                @offenses << "At line #{index + 1}:#{val.size - 1} Trailing Whitespace Detected".colorize(:red)
             end
         end
     end
